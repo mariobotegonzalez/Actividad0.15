47c47217f09b755a043bbcbdd6e471719853a7c4
mariobotegonzalez <mario.botegonzalez.23@campusfp.es>
2024-05-14 11:38:41 +0200

    Update actividad15.sql

--- actividad15.sql
+++ actividad15.sql
@@ -14,3 +14,8 @@ CREATE TABLE clientes (
     ciudad VARCHAR(50),
     facturacion DECIMAL(8,2)
 );
+-- Procedimiento
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_add_clientes`(IN `v_nombre` VARCHAR(50), IN `v_ciudad` VARCHAR(50), IN `v_facturacion` DECIMAL(8,2))
+INSERT INTO `clientes` (`id`, `nombre`, `ciudad`, `facturacion`) VALUES (NULL, v_nombre, v_ciudad, v_facturacion)$$
+DELIMITER ;
